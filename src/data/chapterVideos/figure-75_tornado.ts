3645082c7ac69f72e9f2419a76ff7f5fde93f7c9
Hamatani Yoshitaka <hamatani.yoshitaka@gmail.com>
2025-12-09 16:34:24 +0900

    竜巻　再録

--- src/data/chapterVideos/figure-75_tornado.ts
+++ src/data/chapterVideos/figure-75_tornado.ts
@@ -3,6 +3,5 @@ export const FIGURE_75_TORNADO_VIDEOS = {
   2: require('../../../assets/string-figures/75_tornado/chapters/02.mp4'),
   3: require('../../../assets/string-figures/75_tornado/chapters/03.mp4'),
   4: require('../../../assets/string-figures/75_tornado/chapters/04.mp4'),
-  5: require('../../../assets/string-figures/75_tornado/chapters/05.mp4'),
-  6: require('../../../assets/string-figures/75_tornado/chapters/06.mp4')
+  5: require('../../../assets/string-figures/75_tornado/chapters/05.mp4')
 };
