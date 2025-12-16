b2fd3f7327895bb306bcd10c9e0e3bed00e9f233
Hamatani Yoshitaka <hamatani.yoshitaka@gmail.com>
2025-12-16 17:05:52 +0900

    投げやり 再録

--- src/data/chapterVideos/figure-22_spear.ts
+++ src/data/chapterVideos/figure-22_spear.ts
@@ -1,5 +1,6 @@
 export const FIGURE_22_SPEAR_VIDEOS = {
   1: require('../../../assets/string-figures/22_spear/chapters/01.mp4'),
   2: require('../../../assets/string-figures/22_spear/chapters/02.mp4'),
-  3: require('../../../assets/string-figures/22_spear/chapters/03.mp4')
+  3: require('../../../assets/string-figures/22_spear/chapters/03.mp4'),
+  4: require('../../../assets/string-figures/22_spear/chapters/04.mp4')
 };
