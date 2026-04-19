3deea0b0f908f4368a29067eb25b9d0f1345e7e6
Hamatani Yoshitaka <hamatani.yoshitaka@gmail.com>
2026-04-19 11:46:37 +0900

    かんたんほうきをNonverbal仕様に

--- src/data/chapterVideos/figure-13_easy-broom.ts
+++ src/data/chapterVideos/figure-13_easy-broom.ts
@@ -24,4 +24,33 @@ export const FIGURE_13_EASY_BROOM_NONVERBAL_VIDEO_PAIRS = {
   },
 };
 
+export const FIGURE_13_EASY_BROOM_NONVERBAL_CHAPTER_STILL_PAIRS = [
+  {
+    primary: require('../../../assets/string-figures/13_easy-broom/chapters/01-1.jpg'),
+    secondary: require('../../../assets/string-figures/13_easy-broom/chapters/01-1.jpg'),
+    standby: require('../../../assets/string-figures/13_easy-broom/chapters/01-1.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/13_easy-broom/chapters/02-1.jpg'),
+    secondary: require('../../../assets/string-figures/13_easy-broom/chapters/02-1.jpg'),
+    standby: require('../../../assets/string-figures/13_easy-broom/chapters/00.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/13_easy-broom/chapters/03-1.jpg'),
+    secondary: require('../../../assets/string-figures/13_easy-broom/chapters/03-1.jpg'),
+    standby: require('../../../assets/string-figures/13_easy-broom/chapters/01-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/13_easy-broom/chapters/04-1.jpg'),
+    secondary: require('../../../assets/string-figures/13_easy-broom/chapters/04-1.jpg'),
+    standby: require('../../../assets/string-figures/13_easy-broom/chapters/02-2.jpg'),
+  },
+  /** 再生は最終章まで。ストリップ末尾のプレビュー用に直前スロットと別画像 */
+  {
+    primary: require('../../../assets/string-figures/13_easy-broom/chapters/04-1.jpg'),
+    secondary: require('../../../assets/string-figures/13_easy-broom/chapters/04-1.jpg'),
+    standby: require('../../../assets/string-figures/13_easy-broom/chapters/03-2.jpg'),
+  },
+] as const;
+
 export const FIGURE_13_EASY_BROOM_NONVERBAL_TOTAL_CHAPTERS = 4;
