4c65bf3f3a2c844ad031314e15efd9a417f7ebff
Hamatani Yoshitaka <hamatani.yoshitaka@gmail.com>
2026-04-27 03:35:29 +0900

    火山 nonverbal化

--- src/data/chapterVideos/figure-4_volcano.ts
+++ src/data/chapterVideos/figure-4_volcano.ts
@@ -12,3 +12,160 @@ export const FIGURE_4_VOLCANO_VIDEOS = {
   11: require('../../../assets/string-figures/4_volcano/chapters/11.mp4'),
   12: require('../../../assets/string-figures/4_volcano/chapters/12.mp4'),
 };
+
+export const FIGURE_4_VOLCANO_NONVERBAL_VIDEO_PAIRS = {
+  1: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/01-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/01-2.mp4'),
+  },
+  2: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/02-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/02-2.mp4'),
+  },
+  3: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/03-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/03-2.mp4'),
+  },
+  4: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/04-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/04-2.mp4'),
+  },
+  5: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/05-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/05-2.mp4'),
+  },
+  6: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/06-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/06-2.mp4'),
+  },
+  7: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/07-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/07-2.mp4'),
+  },
+  8: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/08-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/08-2.mp4'),
+  },
+  9: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/09-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/09-2.mp4'),
+  },
+  10: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/10-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/10-2.mp4'),
+  },
+  11: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/11-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/11-2.mp4'),
+  },
+  12: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/12-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/12-2.mp4'),
+  },
+  13: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/13-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/13-2.mp4'),
+  },
+  14: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/14-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/14-2.mp4'),
+  },
+  15: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/15-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/15-2.mp4'),
+  },
+  16: {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/16-1.mp4'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/16-2.mp4'),
+  },
+};
+
+export const FIGURE_4_VOLCANO_NONVERBAL_TOTAL_CHAPTERS = 16;
+
+export const FIGURE_4_VOLCANO_NONVERBAL_CHAPTER_STILL_PAIRS = [
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/01-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/01-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/01-1.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/02-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/02-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/00.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/03-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/03-1-2.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/01-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/04-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/04-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/02-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/05-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/05-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/03-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/06-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/06-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/04-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/07-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/07-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/05-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/08-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/08-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/06-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/09-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/09-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/07-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/10-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/10-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/08-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/11-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/11-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/09-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/12-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/12-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/10-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/13-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/13-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/11-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/14-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/14-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/12-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/15-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/15-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/13-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/16-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/16-1.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/14-2.jpg'),
+  },
+  {
+    primary: require('../../../assets/string-figures/4_volcano/chapters/16-1.jpg'),
+    secondary: require('../../../assets/string-figures/4_volcano/chapters/16-2.jpg'),
+    standby: require('../../../assets/string-figures/4_volcano/chapters/15-2.jpg'),
+  },
+] as const;
--- src/data/index.ts
+++ src/data/index.ts
@@ -104,7 +104,7 @@ export const stringFigures: StringFigure[] = [
       author: null,
       references: null
     },
-    nonverbalFormat: false
+    nonverbalFormat: true
   },
   {
     id: '78',
